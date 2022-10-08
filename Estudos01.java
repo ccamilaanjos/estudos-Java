e6105bee14c55d4d22aa96ccf669870e1cf5f88b
ccamilaanjos <anjoscam.irl@gmail.com>
2022-10-07 23:39:51 -0300

    adiciona anotações sobre tipos de variável

--- Estudos01.java
+++ Estudos01.java
@@ -1,5 +1,16 @@
 public class Estudos01{
     public static void main(String[]args){
-        System.out.println("Teste!");
+        // data types:
+        // int, byte, short, long, float, double, boolean, char
+
+        // Variables // Ex.:
+        // float pi = 3.14F
+        // long cpf = 98765432100L
+        // pi = 3.1415;
+
+        // Constants // Ex.:
+        // final type VARIABLE_NAME;
+        // final int num = 5;
+
     }
-}
+} 
