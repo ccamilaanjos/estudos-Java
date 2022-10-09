cb7dfe45a6aceecb105ee849cdd30855d944157a
ccamilaanjos <anjoscam.irl@gmail.com>
2022-10-08 22:51:47 -0300

    adiciona exercício simples de cálculo de média

--- Estudos01.java
+++ Estudos01.java
@@ -11,6 +11,23 @@ public class Estudos01{
         // Constants // Ex.:
         // final type VARIABLE_NAME;
         // final int num = 5;
+        
+        // Fazer um programa que imprima a média aritmética dos números 8,9 e 7. A média dos
+        // números 4, 5 e 6. A soma das duas médias. A média das médias.
+
+        double media1, media2, mediaMedias;
+        media1 = (8 + 9 + 7) / 3.0;
+        media2 = (4 + 5 + 6) / 3.0;
+
+        double soma;
+
+        soma = media1 + media2;
+        mediaMedias = soma / 2;
+
+        System.out.println("Média aritmética de 8, 9 e 7: " + media1);
+        System.out.println("Média aritmética dos números 4, 5 e 6: " + media2);
+        System.out.println("Soma das médias: " + soma);
+        System.out.println("Média das médias: " + mediaMedias);
 
     }
 } 
